1c130000b52dada3b5a6ec4928c70df31bc42c8d
Felippe <felippe_nvieira@hotmail.com>
2022-02-25 14:02:02 -0300

    Calling functions

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,11 +9,22 @@ void Menu()
     Console.WriteLine("2 - Subtração");
     Console.WriteLine("3 - Divisão");
     Console.WriteLine("4 - Multiplicação");
+    Console.WriteLine("5 - Sair");
 
     Console.WriteLine("--------------------");
 
     Console.WriteLine("Selecione uma opção: ");
     short res = short.Parse(Console.ReadLine());
+
+    switch (res)
+    {
+        case 1: Sum(); break;
+        case 2: Subtraction(); break;
+        case 3: Division(); break;
+        case 4: Multiplication(); break;
+        case 5: System.Environment.Exit(0); break;
+        default: Menu(); break;
+    }
 }
 
 void Sum()
@@ -34,6 +45,7 @@ void Sum()
     //Console.WriteLine($"O resultado da soma é {v1 + v2}");
     //Console.WriteLine("O resultado da soma é " + (v1 + v2));
     Console.ReadKey();
+    Menu();
 }
 
 void Subtraction()
@@ -42,7 +54,7 @@ void Subtraction()
 
     Console.WriteLine("Primeiro Valor: ");
     float v1 = float.Parse(Console.ReadLine());
-    
+
     Console.WriteLine("Segundo Valor: ");
     float v2 = float.Parse(Console.ReadLine());
 
@@ -50,7 +62,10 @@ void Subtraction()
 
     float result = v1 - v2;
     Console.WriteLine($"O resultado da subtração é: {result}");
+    
     Console.ReadKey();
+    Menu();
+
 }
 
 void Division()
@@ -59,7 +74,7 @@ void Division()
 
     Console.WriteLine("Primeiro valor:");
     float v1 = float.Parse(Console.ReadLine());
-    
+
     Console.WriteLine("Segundo valor:");
     float v2 = float.Parse(Console.ReadLine());
 
@@ -69,6 +84,7 @@ void Division()
     Console.WriteLine($"O resultado da divisão é {result}");
 
     Console.ReadKey();
+    Menu();
 }
 
 void Multiplication()
@@ -87,4 +103,5 @@ void Multiplication()
     Console.WriteLine($"O resultado da multiplicação é: {result}");
 
     Console.ReadKey();
+    Menu();
 }
