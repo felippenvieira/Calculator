f7e349b549560c8b6ef8a16cbcfd12d16442cff2
Felippe <felippe_nvieira@hotmail.com>
2022-02-25 13:23:04 -0300

    Sum created

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,15 @@
 ﻿Console.Clear();
+
 Console.WriteLine("Primeiro valor: ");
 float v1 = float.Parse(Console.ReadLine());
-Console.WriteLine(v1);
+
+Console.WriteLine("Segundo valor: ");
+float v2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+float result = v1 + v2;
+Console.WriteLine("O resultado da soma é " + result);
+Console.WriteLine($"O resultado da soma é {result}");
+Console.WriteLine($"O resultado da soma é {v1 + v2}");
+Console.WriteLine("O resultado da soma é " + (v1 + v2));
